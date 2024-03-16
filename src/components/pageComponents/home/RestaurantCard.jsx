e9e0f9e1800db8b8a8541612e99e375e0c54adf9
netojaycee <117773169+netojaycee@users.noreply.github.com>
2024-03-16 18:20:48 +0100

    toaster added for wishlist

--- src/components/pageComponents/home/RestaurantCard.jsx
+++ src/components/pageComponents/home/RestaurantCard.jsx
@@ -7,11 +7,11 @@ import {
   Button,
 } from "@material-tailwind/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock, faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as faHeartOutline } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faClockFour, faHeart as faHeartOutline } from "@fortawesome/free-regular-svg-icons";
 import { WishContext } from "../../../context/WishContext";
 import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function RestaurantCard({ restaurant }) {
   const [isFavorite, setIsFavorite] = useState(false);
@@ -27,46 +27,43 @@ export default function RestaurantCard({ restaurant }) {
       // If already in wishlist, remove from wishlist
       removeFromWish(restaurant.id);
       toast.success(`${restaurant.name} removed from wishlist`);
-
     }
   };
 
   return (
     <>
-    <Card className="mt-6 w-[200px] shadow-lg shadow-gray-600 ">
-      <CardHeader color="blue-gray" className="">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody className="flex flex-col gap-2 p-2">
-        <div className="flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray">
-            {restaurant.name}
-          </Typography>
-          <FontAwesomeIcon
-            icon={isFavorite ? faHeart : faHeartOutline}
-            style={{ color: isFavorite ? "red" : "gray" }}
-            size="lg"
-            className="cursor-pointer"
-            onClick={toggleFavorite}
+      <Card className="mt-6 w-[200px] shadow-lg shadow-gray-600 ">
+        <CardHeader color="blue-gray" className="">
+          <img
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            alt="card-image"
           />
-        </div>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-2 p-2">
+          <div className="flex items-center justify-between">
+            <Typography variant="h5" color="blue-gray">
+              {restaurant.name}
+            </Typography>
+            <FontAwesomeIcon
+              icon={isFavorite ? faHeart : faHeartOutline}
+              style={{ color: isFavorite ? "red" : "gray" }}
+              size="lg"
+              className="cursor-pointer"
+              onClick={toggleFavorite}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <FontAwesomeIcon icon={faClock} style={{ color: "red" }} />
-          <Typography variant="small" color="blue-gray">
-            10 - 25 mins
-          </Typography>
-        </div>
-        <Button color="gray" ripple="light">
-          Visit now
-        </Button>
-      </CardBody>
-      
-    </Card>
-    
+          <div className="flex items-center gap-2">
+            <FontAwesomeIcon icon={faClockFour} style={{ color: "red" }} />
+            <Typography variant="small" color="blue-gray">
+              10 - 25 mins
+            </Typography>
+          </div>
+          <Button color="gray" ripple="light">
+            Visit now
+          </Button>
+        </CardBody>
+      </Card>
     </>
   );
 }
